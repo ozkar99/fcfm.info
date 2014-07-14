8d32e97fd07d5365d0fdf7f346ae471c37425a87
Oscar Moreno Garza <oscarmg99@gmail.com>
2014-07-13 19:16:26 -0500

    Created placeholder menu to start working on other things.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FcfmInfo::Application.config.secret_token = 'c1a8b95e030df4729c5a458f160d0e8ba099d7629a14907ade64e7a19a75e6021b10fde69bb02bffa459cc0d298f180b569ee323edbd265bbdeef0a612ce1fb6'
+FcfmInfo::Application.config.secret_token = 'cu4iKEu4sRU0m800e8ZaSoA84Gij0t008VT2u87C3bSDFbx254qKMguvn6jRU884u4F90t5Vlg047ly5627lEiTrm1VENTMd'
